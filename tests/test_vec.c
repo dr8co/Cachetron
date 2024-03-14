23623ba8dfb453399376cc4aee043aa9fa33f55f
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2024-03-15 00:17:21 +0300

    Add nullptr tests for vector

--- tests/test_vec.c
+++ tests/test_vec.c
@@ -97,6 +97,49 @@ void vector_clear_resets_size() {
     vector_free(v);
 }
 
+void vector_size_returns_zero_for_nullptr() {
+    assert(vector_size(nullptr) == 0);
+}
+
+void vector_capacity_returns_zero_for_nullptr() {
+    assert(vector_capacity(nullptr) == 0);
+}
+
+void vector_push_back_does_not_crash_for_nullptr() {
+    constexpr int value = 5;
+    vector_push_back(nullptr, &value);
+}
+
+void vector_at_returns_null_for_nullptr() {
+    assert(vector_at(nullptr, 0) == nullptr);
+}
+
+void vector_pop_back_does_not_crash_for_nullptr() {
+    vector_pop_back(nullptr);
+}
+
+void vector_append_does_not_crash_for_nullptr() {
+    constexpr int values[] = {1, 2, 3, 4, 5};
+    vector_append(nullptr, values, 5);
+}
+
+void vector_insert_does_not_crash_for_nullptr() {
+    constexpr int value = 5;
+    vector_insert(nullptr, 0, &value);
+}
+
+void vector_erase_does_not_crash_for_nullptr() {
+    vector_erase(nullptr, 0);
+}
+
+void vector_resize_expand_does_not_crash_for_nullptr() {
+    vector_resize_expand(nullptr, 20);
+}
+
+void vector_clear_does_not_crash_for_nullptr() {
+    vector_clear(nullptr);
+}
+
 int main() {
     vector_new_creates_empty_vector();
     vector_push_back_increases_size();
@@ -109,5 +152,16 @@ int main() {
     vector_erase_decreases_size();
     vector_resize_expand_increases_size();
     vector_clear_resets_size();
+
+    vector_size_returns_zero_for_nullptr();
+    vector_capacity_returns_zero_for_nullptr();
+    vector_push_back_does_not_crash_for_nullptr();
+    vector_at_returns_null_for_nullptr();
+    vector_pop_back_does_not_crash_for_nullptr();
+    vector_append_does_not_crash_for_nullptr();
+    vector_insert_does_not_crash_for_nullptr();
+    vector_erase_does_not_crash_for_nullptr();
+    vector_resize_expand_does_not_crash_for_nullptr();
+    vector_clear_does_not_crash_for_nullptr();
     return 0;
 }
