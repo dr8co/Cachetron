5ab8f1f4ff65723bda3643f00ee089a04e95ebbe
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2024-03-17 23:02:30 +0300

    Update client to send basic server commands

--- client.cpp
+++ client.cpp
@@ -7,6 +7,9 @@
 #include <assert.h>
 #include <errno.h>
 
+#include "string.h"
+#include "vector.h"
+
 #if __GNUC__ >= 13 || __clang_major__ >= 19
 constexpr size_t k_max_msg = 4096;
 #else
@@ -52,13 +55,30 @@ static int32_t write_all(const int fd, const char *buf, size_t n) {
     return 0;
 }
 
-static int32_t send_req(const int fd, const char *text) {
-    const uint32_t len = strlen(text);
+static int32_t send_req(const int fd, const ptr_vector *cmd) {
+    uint32_t len = 4;
+
+    for (size_t i = 0; i < ptr_vector_size(cmd); ++i) {
+        len += 4 + string_length((string *) ptr_vector_at(cmd, i));
+    }
     if (len > k_max_msg) return -1;
 
     char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4); // assume little endian
-    memcpy(&wbuf[4], text, len);
+    memcpy(&wbuf[0], &len, 4); // assume little endian
+    const uint32_t n = ptr_vector_size(cmd);
+    memcpy(&wbuf[4], &n, 4);
+    size_t cur = 8;
+
+    for (size_t i = 0; i < ptr_vector_size(cmd); ++i) {
+        char *c_str = string_cstr((string *) ptr_vector_at(cmd, i));
+        uint32_t p = string_length((string *) ptr_vector_at(cmd, i));
+        memcpy(&wbuf[cur], &p, 4);
+
+        memcpy(&wbuf[cur + 4], c_str, string_length((string *) ptr_vector_at(cmd, i)));
+        cur += 4 + string_length((string *) ptr_vector_at(cmd, i));
+        free(c_str);
+    }
+
     return write_all(fd, wbuf, 4 + len);
 }
 
@@ -86,13 +106,18 @@ static int32_t read_res(const int fd) {
         return err;
     }
 
-    // do something
-    rbuf[4 + len] = '\0';
-    printf("server says: %s\n", &rbuf[4]);
+    // print the result
+    uint32_t rescode = 0;
+    if (len < 4) {
+        report_error("bad response");
+        return -1;
+    }
+    memcpy(&rescode, &rbuf[4], 4);
+    printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
     return 0;
 }
 
-int main() {
+int main(int argc, char **argv) {
     const int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) die("socket() failure");
 
@@ -104,18 +129,23 @@ int main() {
     const int rv = connect(fd, (const struct sockaddr *) &addr, sizeof addr);
     if (rv) die("connect() failure");
 
-    // multiple pipelined requests
-    const char *query_list[3] = {"hello1", "hello2", "hello3"};
-    for (size_t i = 0; i < 3; ++i) {
-        const int32_t err = send_req(fd, query_list[i]);
-        if (err) goto L_DONE;
+    ptr_vector *cmd = ptr_vector_new();
+
+    for (int i = 1; i < argc && i < ptr_vector_capacity(cmd); ++i) {
+        ptr_vector_push_back(cmd, string_new());
+        string_append_cstr((string *) ptr_vector_at(cmd, i - 1), argv[i]);
     }
-    for (size_t i = 0; i < 3; ++i) {
-        const int32_t err = read_res(fd);
-        if (err) goto L_DONE;
+    int32_t err = send_req(fd, cmd);
+    if (err) {
+        goto L_DONE;
     }
+    read_res(fd);
 
 L_DONE:
     close(fd);
+    for (size_t i = 0; i < ptr_vector_size(cmd); ++i)
+        string_free((string *) ptr_vector_at(cmd, i));
+
+    ptr_vector_free(cmd);
     return 0;
 }
